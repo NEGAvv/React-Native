fa3103cdfd7c29470d59241d1d254aa48b34eb57
NEGAvv <hudolija@gmail.com>
2023-10-13 11:08:07 +0300

    validationOfInput

--- components/Task/Task.js
+++ components/Task/Task.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, TextInput } from 'react-native';
-import styles from './styles'
-import Table from '../Table/Table'
+import styles from './styles';
+import Table from '../Table/Table';
 
 const Task = () => {
   const [red, setRed] = useState('255');
@@ -13,15 +13,15 @@ const Task = () => {
     return { backgroundColor };
   };
 
-  const [tableSize, setTableSize] = useState({ width: 25, height: 25 }); 
+  const [tableSize, setTableSize] = useState({ width: 25, height: 25 });
 
   const increaseSize = () => {
-      if (tableSize.width < 115 || tableSize.height < 115) {
-        setTableSize({
-          width: tableSize.width + 10,
-          height: tableSize.height + 10,
-        });
-      }
+    if (tableSize.width < 115 || tableSize.height < 115) {
+      setTableSize({
+        width: tableSize.width + 10,
+        height: tableSize.height + 10,
+      });
+    }
   };
 
   const decreaseSize = () => {
@@ -32,46 +32,55 @@ const Task = () => {
       });
     }
   };
-  
+
+  const handleInputChange = (value, setValue) => {
+    if (/^\d{0,3}$/.test(value)) {
+      if (parseInt(value, 10) > 255) {
+        setValue('255');
+      } else if (value >= 0) {
+        setValue(value);
+      }
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={[styles.main, handleColorChange()]}>
-          <Table width={tableSize.width} height={tableSize.height}/>
+        <Table width={tableSize.width} height={tableSize.height} />
 
         <View style={styles.buttonGroup}>
           <TouchableOpacity style={styles.button} onPress={increaseSize}>
-              <Text style={styles.textButton}>+</Text>
+            <Text style={styles.textButton}>+</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={decreaseSize}>
-              <Text style={styles.textButton}>-</Text>
+            <Text style={styles.textButton}>-</Text>
           </TouchableOpacity>
         </View>
 
         <Text>Write a number</Text>
         <View style={styles.inputGroup}>
-          <TextInput 
-            style={styles.input} 
-            onChangeText={(text) => setRed(text)}
-            keyboardType="numeric">
-          </TextInput>
-          <TextInput 
+          <TextInput
             style={styles.input}
-            onChangeText={(text) => setGreen(text)}
-            keyboardType="numeric">
-            </TextInput>
-          <TextInput 
+            onChangeText={(text) => handleInputChange(text, setRed)}
+            keyboardType="numeric"
+            value={red}
+          />
+          <TextInput
             style={styles.input}
-            onChangeText={(text) => setBlue(text)}
-            keyboardType="numeric">
-          </TextInput>
+            onChangeText={(text) => handleInputChange(text, setGreen)}
+            keyboardType="numeric"
+            value={green}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => handleInputChange(text, setBlue)}
+            keyboardType="numeric"
+            value={blue}
+          />
         </View>
-
       </View>
     </View>
-  )
-}
-
-
+  );
+};
 
 export default Task;
